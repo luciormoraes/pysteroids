0f37a2301dcba38323b58b59e041f84ea40c9a21
Lucio Moraes <luciomoraes@gmail.com>
2025-02-03 22:07:30 +0100

    Modifying main.py to add a simple fps. adding constants to hold values, adding player.py and circleshape to draw sprites on screen, and control the movements of player."

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
 
@@ -13,14 +14,30 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids Game")
 
+    clock = pygame.time.Clock()  # Control frame rate
+    dt = 0
+    running = True
     colour = (0,0,0)
 
-    while True:
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(colour)
+
+        player.draw(screen)
+
         pygame.display.flip()
+        player.update(dt)
+
+        res = clock.tick(60)
+        dt = res / 1000
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
